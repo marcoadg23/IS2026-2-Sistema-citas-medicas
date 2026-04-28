c2af633f1681fef76cf49f94682f19eb2e61762d
marcoadg23 <marcoadg23@gmail.com>
2026-04-28 16:04:33 -0600

    init: Primera versión de login

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function login(){
+    let divMensaje = document.querySelector('#mensaje');
+    
+    let usuario = document.querySelectorAll('.entradaTexto')[0].value;
+    let contra = document.querySelectorAll('.entradaTexto')[1].value;
+
+    if(usuario=="admin" && contra=="123"){
+        window.location.href="/pages/principal.html";
+    }else{
+        divMensaje.innerHTML = "Credenciales incorrectas";
+    }
+
+    
+}
+
+const btnIngresar = document.getElementById('btnIngresar');
+ btnIngresar.addEventListener("click",login)
+
+
